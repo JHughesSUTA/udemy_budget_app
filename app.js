ba4f281828fead543cf7b6c423211bedb0e478b3
James Hughes <james.a.hughes@gmail.com>
2018-02-14 22:14:32 -0600

    Capture field input data, allow controller to access it

--- app.js
+++ app.js
@@ -6,38 +6,47 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
-    //code goes here
+    
+    //not a private function, because we want to use it in the other controllers
+    return {
+        getInput: function(){
+            // capture the values for the three parts of the form
+            return {
+                type: document.querySelector('.add__type').value, //value will be 'inc' or 'exp' (look at the html)
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value
+            };
+        }
+    };
+
 })();
 
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-
     var ctrlAddItem = function() {
         // this function will need to: 
         // 1. Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
+
         // 2. Add the item to the budget controller
+
         // 3. Add the new item to the UI
+
         // 4. Calculate the budget
+
         // 5. Display the budget on the UI
-        console.log('test');
     }
 
-
-
-    /* event listener for clicking the add button. We can get rid of the anonymous function, because we can now pass in the functionw defined above, ctrlAddItem,  as an argument: */
     document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
-
-
-    /* event listener for hitting enter (we don't need a query selector because it's global, rather than for a certain element). 
-    'keypress' event is for ANY key, so we need to put an if statement so it only works if enter is pressed. 13 is the keycode for ENTER... some browsers use 'which' instead of keycode - so we add the 'or'
-    */
     document.addEventListener('keypress', function(event) {
         if (event.keyCode === 13 || event.which === 13) {
             ctrlAddItem();
-        }        
+        }  
+
     });
 
 })(budgetController, UIController);
